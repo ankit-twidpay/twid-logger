99f92bd43e56e4729a5b62ab2fa7a5045c92f3ab
ankit-twidpay <ankit.tiwari@twidpay.com>
2023-10-01 17:29:52 +0530

    added documentation

--- src/Facades/TLog.php
+++ src/Facades/TLog.php
@@ -5,10 +5,39 @@ namespace twid\logger\Facades;
 use Illuminate\Support\Facades\Facade;
 use twid\logger\Logger;
 
+/**
+ * TLog Facade for logging messages.
+ *
+ * This facade provides a dynamic method for logging messages, where the method name is treated as the log channel.
+ * If the method is not explicitly defined, it is treated as a channel name, and the log method is invoked on the corresponding logger instance.
+ *
+ * @method static bool log(string $message, array $data = []) Log a message with the specified data.
+ * @method static bool info(string $message, array $data = []) Log an information message.
+ * @method static bool warning(string $message, array $data = []) Log a warning message.
+ * @method static bool critical(string $message, array $data = []) Log a critical message.
+ * @method static bool error(string $message, array $data = []) Log an error message.
+ * @method static bool alert(string $message, array $data = []) Log an alert message.
+ * @method static bool inbound(string $message, array $data = []) Log an inbound message.
+ * @method static bool outbound(string $message, array $data = []) Log an outbound message.
+ * @method static bool debug(string $message, array $data = []) Log a debug message.
+ */
 class TLog extends Facade
 {
+    /**
+     * Associative array to store logger instances based on channels.
+     *
+     * @var array
+     */
     protected static $loggers = [];
 
+
+    /**
+     * Get the logger instance based on the channel name.
+     *
+     * @param string $channel The log channel.
+     *
+     * @return Logger Logger instance.
+     */
     protected static function getLogger($channel = 'default')
     {
         if (!isset(self::$loggers[$channel])) {
@@ -18,12 +47,24 @@ class TLog extends Facade
         return self::$loggers[$channel];
     }
 
+    /**
+     * Handle dynamic, static calls to the object.
+     *
+     * @param string $method The method being called.
+     * @param array  $args   Arguments passed to the method.
+     *
+     * @return mixed The result of the method call.
+     */
     public static function __callStatic($method, $args)
     {
-        // If the method is not explicitly defined, treat it as a channel name
         return self::getLogger($method)->log($args[0], $args[1] ?? []);
     }
 
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
     protected static function getFacadeAccessor()
     {
         return 'twid.logger';
--- src/logger.php
+++ src/logger.php
@@ -6,32 +6,76 @@ use Monolog\Formatter\JsonFormatter;
 use Monolog\Handler\RotatingFileHandler;
 use Monolog\Logger as MonologLogger;
 
+/**
+ * Logger class for handling log messages.
+ */
 class Logger
 {
+    /**
+     * The Monolog logger instance.
+     *
+     * @var MonologLogger
+     */
     protected $log;
+
+    /**
+     * Configuration settings for the logger.
+     *
+     * @var array
+     */
     protected $config;
-    protected $request;
+
+    /**
+     * The current log channel.
+     *
+     * @var string
+     */
     protected $channel;
+
+    /**
+     * Metadata to be included in log entries.
+     *
+     * @var array
+     */
     protected $metadata = [];
 
+    /**
+     * Logger constructor.
+     *
+     * @param string $channel The log channel.
+     *
+     * @throws \InvalidArgumentException If channel configuration is not found.
+     */
     public function __construct($channel = 'default')
     {
         $this->log = new MonologLogger($channel);
+
+        // Load configuration settings for the logger.
         $this->config = include(dirname(__DIR__, 4) . '/config/logging.php');
         $this->channel = $channel;
-
         $channelConfig = $this->config['channels'][$channel];
 
-        if ($channelConfig) {
-            $logPath = $channelConfig['path'];
-            $logLevel = $channelConfig['level'];
-
-            $handler = new RotatingFileHandler($logPath, 0, $logLevel);
-            $handler->setFormatter(new JsonFormatter());
-
-            $this->log->pushHandler($handler);
+        if (!$channelConfig) {
+            throw new \InvalidArgumentException("Channel configuration for '$channel' not found."); //@todo check with kushal if this needs to be provided or not
         }
+
+        $logPath = $channelConfig['path'];
+        $logLevel = $channelConfig['level'];
+
+        $handler = new RotatingFileHandler($logPath, 0, $logLevel);
+        $handler->setFormatter(new JsonFormatter());
+
+        $this->log->pushHandler($handler);
     }
+
+    /**
+     * Log a message with the specified data.
+     *
+     * @param string $message The log message.
+     * @param array  $data    Additional data to be included in the log entry.
+     *
+     * @return bool True on success, false on failure.
+     */
     public function log($message, $data = [])
     {
         $data = $this->maskFields($data);
@@ -42,6 +86,11 @@ class Logger
         return $this->log->log($this->config['channels'][$this->channel]['level'], $message, $data);
     }
 
+    /**
+     * Retrieve metadata based on configuration settings.
+     *
+     * @return array Metadata to be included in log entries.
+     */
     protected function metadata()
     {
         $defaultLog = $this->config['metadata'];
@@ -53,6 +102,14 @@ class Logger
 
         return $this->maskFields($this->metadata);
     }
+
+    /**
+     * Mask sensitive fields in the provided data.
+     *
+     * @param array $data Data to be masked.
+     *
+     * @return array Masked data.
+     */
     protected function maskFields($data = [])
     {
         $fields = $this->config['mask'];
